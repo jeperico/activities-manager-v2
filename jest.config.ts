8ecc890dd4e28eba20fe68d664c56e211daf633b
João Périco <joaoeperico@gmail.com>
2025-01-05 02:49:18 -0300

    [feat] trying test

---
+++ jest.config.ts
@@ -0,0 +1,6 @@
+module.exports = {
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
+  },
+};
---
+++ src/app/login/login.spec.tsx
@@ -0,0 +1,46 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+import Login from './page';
+
+jest.mock('@/public/background-login.jpg', () => null);
+
+describe('Login Form', () => {
+  it('should render the login form', () => {
+    // Arrange
+    render(<Login />);
+
+    // Act
+    const inputName = screen.getByPlaceholderText(/user/i);
+    const inputPassword = screen.getByPlaceholderText(/password/i);
+
+    // Assert
+    expect(inputName).toBeVisible();
+    expect(inputPassword).toBeVisible();
+  });
+
+  it('should show an error message when the user is not provided', () => {
+    // Arrange
+    // Act
+    // Assert
+  });
+
+  it('should show an error message when the password is not provided', () => {
+    // Arrange
+    // Act
+    // Assert
+  });
+
+  it('should show an error message when the password is less than 6 characters long', () => {
+    // Arrange
+    // Act
+    // Assert
+  });
+
+  it('should call the handleLoginUser function when the form is submitted', () => {
+    // Arrange
+    // Act
+    // Assert
+  });
+
+});
