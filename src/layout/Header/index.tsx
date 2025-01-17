baf3179585515841e509cc17d11974340a768498
João Périco <joaoeperico@gmail.com>
2025-01-17 04:39:14 -0300

    [feat] fetch data from teacher name

--- src/layout/Header/index.tsx
+++ src/layout/Header/index.tsx
@@ -1,18 +1,38 @@
 "use client";
 
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Logo from "@/public/logo.png";
 import Button from "@/components/Button";
 import Link from "next/link";
+import { get } from "@/providers/api";
 
 const Header = () => {
   const returnHome = true;
   const retunActivities = true;
+  const teacherId = "579c7ca8-014f-4631-baf2-3f42b041c132";
+  const [name, setName] = useState("Teacher");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await get("profiles/teacher/");
+        const teacher = response.filter(
+          (teacher: { id: string; user: { name: string } }) =>
+            teacher.id === teacherId,
+        );
+        setName(teacher[0].user.name);
+      } catch (error) {
+        console.error("Error fetching teacher data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <header className="bg-sesi text-white p-4 grid grid-cols-header items-center">
-      <h1>Name</h1>
+      <h1>{name}</h1>
       <Link href="/">
         <Image
           src={Logo.src}
