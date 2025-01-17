eba777498415f4416aa9a2716f418b44384a7bb3
João Périco <joaoeperico@gmail.com>
2025-01-17 03:53:01 -0300

    [feat] add api provider

---
+++ src/providers/api.ts
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+type HttpMethod = "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
+
+export interface IPostRequestParams {
+  body?: object | string;
+}
+
+const httpRequest = async (
+  uri: string,
+  method: HttpMethod,
+  props: IPostRequestParams = {},
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+): Promise<any> => {
+  const endpoint = `${process.env.NEXT_PUBLIC_API_URL}/${uri}`;
+  const body = props.body ? { data: props.body } : {};
+
+  const params = {
+    url: endpoint,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method,
+    ...body,
+  };
+
+  try {
+    const response = await axios(params);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+console.log(httpRequest("users", "GET"));
