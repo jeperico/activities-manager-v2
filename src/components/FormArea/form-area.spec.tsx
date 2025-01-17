f55f1a098f3ff72d9c29f17973e34dae0c29e94d
João Périco <joaoeperico@gmail.com>
2025-01-17 01:25:03 -0300

    [build] no fucking, I done it

--- src/components/FormArea/form-area.spec.tsx
+++ src/components/FormArea/form-area.spec.tsx
@@ -1,10 +1,9 @@
-/* eslint-disable testing-library/no-unnecessary-act */
-import { render, screen, fireEvent, act } from '@testing-library/react';
-import '@testing-library/jest-dom';
 import { jest } from '@jest/globals';
+import '@testing-library/jest-dom';
+import { act, fireEvent, render, screen } from '@testing-library/react';
 
-import FormArea from '.';
 import { LoginUserSchema } from '@/app/login/login-form';
+import FormArea from '.';
 
 beforeAll(() => {
   HTMLFormElement.prototype.submit = jest.fn();
@@ -16,8 +15,12 @@ describe('Form Area', () => {
     const handleLoginUser = (data: LoginUserSchema) => {
       console.log(data.password);
       console.log(data.user);
-    }
-    render(<FormArea onSubmit={handleLoginUser}><p>Form Area</p></FormArea>);
+    };
+    render(
+      <FormArea onSubmit={handleLoginUser}>
+        <p>Form Area</p>
+      </FormArea>
+    );
 
     // Act
     const formArea = screen.getByText('Form Area');
@@ -29,7 +32,11 @@ describe('Form Area', () => {
   it('should call onSubmit when form is submitted', () => {
     // Arrange
     const mockHandleSubmit = jest.fn();
-    render(<FormArea onSubmit={mockHandleSubmit}><button type="submit">Submit</button></FormArea>);
+    render(
+      <FormArea onSubmit={mockHandleSubmit}>
+        <button type="submit">Submit</button>
+      </FormArea>
+    );
 
     // Act
     const submitButton = screen.getByText('Submit');
@@ -43,7 +50,11 @@ describe('Form Area', () => {
 
   it('should render children elements', () => {
     // Arrange
-    render(<FormArea onSubmit={() => {}}><p>Child Element</p></FormArea>);
+    render(
+      <FormArea onSubmit={() => {}}>
+        <p>Child Element</p>
+      </FormArea>
+    );
 
     // Act
     const childElement = screen.getByText('Child Element');
